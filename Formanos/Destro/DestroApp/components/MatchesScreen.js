502cbeb818de8789ecf9b9962c30991fa5dc9c1f
KristianZednikovic <75309747+KristianZednikovic@users.noreply.github.com>
2023-01-17 22:54:32 +0100

    cards setting updating

--- Formanos/Destro/DestroApp/components/MatchesScreen.js
+++ Formanos/Destro/DestroApp/components/MatchesScreen.js
@@ -1,41 +1,54 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import {StyleSheet, ActivityIndicator, Text, View, Button} from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Text,
+  ScrollView,
+  Button,
+} from 'react-native';
 
 export default function MatchesScreen({navigation, route}) {
-  const [name, setName] = useState('');
-  const [position, setPosition] = useState('');
-  const [kills, setKills] = useState('');
-  const [deaths, setDeaths] = useState('');
-  const [assits, setAssists] = useState('');
+  const [cards, setCard] = useState([]);
+  var karticka = [];
 
   const getMatchInfo = async () => {
     try {
-      const response = await fetch(
-        'https://europe.api.riotgames.com/lol/match/v5/matches/' +
-          route.params.paramKey[0][0] +
-          '?api_key=RGAPI-af06d3b7-85c1-47b7-adf8-7c7b18dba569',
-      );
+      for (let i = 0; i < route.params.paramKey[0].length; i++) {
+        const response = await fetch(
+          'https://europe.api.riotgames.com/lol/match/v5/matches/' +
+            route.params.paramKey[0][i] +
+            '?api_key=RGAPI-af06d3b7-85c1-47b7-adf8-7c7b18dba569',
+        );
 
-      const json = await response.json();
+        const json = await response.json();
 
-      console.log(route.params.paramKey[1]);
+        for (let i = 0; i < 10; i++) {
+          const playerData = json['info']['participants'][i];
+          // if (playerData['summonerName'] == route.params.paramKey[1]) {
+          //   setName(playerData['summonerName']);
+          //   setPosition(playerData['teamPosition']);
+          //   setKills(playerData['kills']);
+          //   setDeaths(playerData['deaths']);
+          //   setAssists(playerData['assists']);
+          // }
 
-      for (let i = 0; i < 10; i++) {
-        const playerData = json['info']['participants'][i];
-        if (playerData['summonerName'] == route.params.paramKey[1]) {
-          setName(playerData['summonerName']);
-          setPosition(playerData['teamPosition']);
-          setKills(playerData['kills']);
-          setDeaths(playerData['deaths']);
-          setAssists(playerData['assists']);
+          karticka = [
+            ...karticka,
+            {
+              key: karticka.length + 1,
+              summonerName: playerData['summonerName'],
+              teamPosition: playerData['teamPosition'],
+              kills: playerData['kills'],
+              deaths: playerData['deaths'],
+              assists: playerData['assists'],
+            },
+          ];
         }
-        console.log('Summoner name:', playerData['summonerName']);
-        console.log('Team position: ', playerData['teamPosition']);
-        console.log('Kills: ', playerData['kills']);
-        console.log('Deaths: ', playerData['deaths']);
-        console.log('Assists: ', playerData['assists']);
-        console.log('');
+
+        setCard(karticka);
       }
+      //--------
     } catch (error) {
       console.error(error);
     } finally {
@@ -47,13 +60,26 @@ export default function MatchesScreen({navigation, route}) {
     getMatchInfo();
   }, []);
 
+  const matchList = cards.map(card => {
+    return (
+      <View style={styles.textView}>
+        <Text>{card.summonerName}</Text>
+        <Text>
+          Kills: {card.kills} Deaths: {card.deaths} Assists: {card.assists}
+        </Text>
+        <Text></Text>
+      </View>
+    );
+  });
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Summoner name: {name}</Text>
-      <Text>Team position: {position}</Text>
-      <Text>Kills: {kills}</Text>
-      <Text>Deaths: {deaths}</Text>
-      <Text>Assists: {assits}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>{matchList}</ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  textView: {alignItems: 'center', justifyContent: 'center'},
+});
